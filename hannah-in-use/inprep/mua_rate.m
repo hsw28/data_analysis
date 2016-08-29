5822698b6a9cc01b5bdc1fb5cb6109dde5e344e6
Hannah Wirtshafter <holdbackaglacier@yahoo.com>
2016-08-29 19:51:03 -0400

    updated mua rate finder to make it faster/more flexible

--- hannah-in-use/inprep/mua_rate.m
+++ hannah-in-use/inprep/mua_rate.m
@@ -1,7 +1,16 @@
-function muar = mua_rate(file, start_time, end_time);
+function muar = mua_rate(file, start_time, end_time, t);
 
-%make a vector of firing times
+% finds rate of MUA
+% function muar = mua_rate(file, start_time, end_time, t);
+%
+% file should be the loaded file
+% t is bin in seconds (for ex, .01 for 10ms)
+%
+% ex:
+% r = mua_rate(maze_cl_2, 455.8529, 24855.7439, .01);
+
 tv = file(:,8);
+%make a vector of firing times
 
 ms_time = (end_time-start_time);
 time_v = [];
@@ -10,8 +19,8 @@ k = start_time;
 %going through all the time and making a vector time_v with each millisecond
 while k <= end_time
 	time_v(end+1) = k;
-	%k = k+.001;
-	k = k+1;
+	k = k+t;
+	%k = k+1;
 end
 
 
@@ -19,20 +28,21 @@ end
 rate = zeros(size(time_v));
 
 m = size((time_v),2)
+s = 1;
+q = 1;
 
 %for the full length of time, go through all the time points and see if they fit in the ms bin
 % if they fit, add a tally in the rate vector for that time point
 for n = 1:(m-1)
 	r = 0;
-	for s = 1:(size((tv),1))
-		s;
-		tv(s);
-		time_v(n);
-		time_v(n+1);
+	while s <= (size((tv),1))
 		if tv(s) >= time_v(n) && tv(s) < time_v(n+1)
 			rate(n) = rate(n)+1;
+			q = s;
 		end
+		s = s+1;
 	end
+	s = q;
 end
 
 muar = rate;
