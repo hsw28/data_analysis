8121c6ac310d6b044dd512df7005064bb0496366
hsw28 <holdbackaglacier@yahoo.com>
2016-08-30 17:42:15 -0400

    working now, was missing some data points before. still might be missing 1 point but only tried on very large time scale.

--- hannah-in-use/inprep/mua_rate.m
+++ hannah-in-use/inprep/mua_rate.m
@@ -27,29 +27,38 @@ end
 %make empty rate vector
 rate = zeros(size(time_v));
 
-m = size((time_v),2)
+m = size((time_v),2);
 s = 1;
 n = 1;
+q = 1;
 
 %for the full length of time, go through all the time points and see if they fit in the ms bin
 % if they fit, add a tally in the rate vector for that time point
 
-while n <= (m-1) %for all the times
-	while s <= (size((tv),1)) %go through the spikes
-		if tv(s) >= time_v(n) && tv(s) < time_v(n+1) %if the spike S is within the time window
-			rate(n) = rate(n)+1; %add a tally to the rate vector
-			s = s+1; %go to next spike
-		elseif tv(s) >= time_v(n+1)
-			s = s+1;
+while n <= (m) %for all the times
+	while s < (size((tv),1)) %go through the spikes
+		if n < m		
+			if tv(s) >= time_v(n) && tv(s) < time_v(n+1) %if the spike S is within the time window
+				rate(n) = rate(n)+1; %add a tally to the rate vector
+				q=s;
+			end
 		end
+		if n == m
+			if tv(s) >= time_v(n)
+				rate(n) = rate(n)+1;
+			end
+		end
+		s = s+1;
 	end
-	n=n+1;
+	n=n+1
+	s=q;
+
 end
 
 muar = rate;
 			
 	
-	
+% time one, go through all spikes and see if match	
 
 
 
