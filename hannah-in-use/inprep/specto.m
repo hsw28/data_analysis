2df2aab7f6e6aa972656c6096275510802c59f14
Hannah Wirtshafter <holdbackaglacier@yahoo.com>
2016-09-06 20:08:53 -0400

    did some stuff, added spectrogram code

---
+++ hannah-in-use/inprep/specto.m
@@ -0,0 +1,11 @@
+function sp = specto(data);
+
+[s, f, t] = spectrogram(abs(data), 2000, 1000, 1000, 2000);
+S = abs(s);
+
+%imagesc(x,y,C) 
+
+imagesc(t,f,S')
+axis xy
+set(gca,'ylim',[0,300]);
+set(gca,'clim',[0,2]);
