b1f627327764c710f95eb64d0aa83f4c46deba4a
Hannah Wirtshafter <holdbackaglacier@yahoo.com>
2016-09-09 17:09:01 -0400

    added file to find acceleration

--- hannah-in-use/inprep/accel.m
+++ hannah-in-use/inprep/accel.m
@@ -1,15 +1,25 @@
-function a = accel(file1);
+function a = accel(file);
 
 %computes acceleration. input a [2, #of points] vector, where first column is velcotiy, second is time
 %
 % returns acceleration per ms and time stamp vector
 % doesn't smooth or transform-- do that later when you assign velocities
 
-file = file1';
 
 vel = file(1, :);
-tm = file(2, :);
+t = file(2, :);
 
-a = diff(vel, tm);
+accvector = [];
+timevector = [];
+
+s = size(t,2)
+
+for i = 2:s
+	vchange = vel(i)-vel(i-1);
+	accel = vchange/(t(i)-t(i-1));
+	accvector(end+1) = accel;
+	timevector(end+1) = t(i);
+end
 
 
+a = [accvector; timevector];
