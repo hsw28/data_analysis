4cf0f594aa1cefe4575fb3c64ad670ec294845c8
Hannah Wirtshafter <holdbackaglacier@yahoo.com>
2016-09-12 19:38:51 -0400

    added idea for a directional function

---
+++ hannah-in-use/inprep/towardreward.m
@@ -0,0 +1,5 @@
+function f = towardreward(eventpos)
+
+% center stem is about y coordinates 340 to 370
+% subtract surrounding x points and see if animal is going away or towards reward
+% actually could divide this into quadrants and do it over whole area... hmm
