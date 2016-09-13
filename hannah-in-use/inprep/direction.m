6bbd9e8bfdcbb2948c3997c49529a69e60df16d9
Hannah Wirtshafter <holdbackaglacier@yahoo.com>
2016-09-13 15:49:34 -0400

    direction file works... weird that is does on first try

--- hannah-in-use/inprep/direction.m
+++ hannah-in-use/inprep/direction.m
@@ -1,5 +1,10 @@
+function v = direction(event, posfile)
 
-posfile
+% inputs
+% posfile (# points, 3)
+% even time file (1, times)
+% outputs
+% vector = [timevector xposvector yposvector fxvector fyvector];
 
 
 t = posfile(:,1);
@@ -13,6 +18,64 @@ fy = gradient(y);
 tfxfy = [t x y fx fy];
 
 % go through and find times, assign fx fy values and position
-% plot using quiver using subselected event specific x, y, fx, fy
-% quiver(x, y, fx, fy);
+
+eventsize = size(event,2);
+at = size(tfxfy);
+
+timevector = [];
+xposvector = [];
+yposvector = [];
+fxvector = [];
+fyvector = [];
+
+i = 1;
+newn = 1;
+while i <= eventsize
+	m = [];
+	for n = 1:size(t,1)
+		m(end+1) = (abs(event(i)-(t(n))));
+		if size(m,2) == 1
+			time = tfxfy(n,1);
+			xpos = tfxfy(n,2);
+			ypos = tfxfy(n,3);
+			sfx = tfxfy(n,4);
+			sfy = tfxfy(n,5);
+			timevector(i) = time;
+			xposvector(i) = xpos;
+			yposvector(i) = ypos;
+			fxvector(i) = sfx;
+			fyvector(i) = sfy;
+		end
+
+		if size(m,2)>1
+			if m(end)<=m(end-1)
+				time = tfxfy(n,1);
+				xpos = tfxfy(n,2);
+				ypos = tfxfy(n,3);
+				sfx = tfxfy(n,4);
+				sfy = tfxfy(n,5);
+				timevector(i) = time;
+				xposvector(i) = xpos;
+				yposvector(i) = ypos;
+				fxvector(i) = sfx;
+				fyvector(i) = sfy;
+				newn=n;
+			end
+
+			if m(end)>m(end-1)
+				break
+			end
+		end	
+	end
+
+i=i+1;
+
+end
+
+image
+quiver(xposvector, yposvector, fxvector, fyvector)
+v = [timevector; xposvector; yposvector; fxvector; fyvector];
+
+
+
 
