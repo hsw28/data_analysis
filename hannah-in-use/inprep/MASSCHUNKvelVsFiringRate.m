16bdecbc8a552b3bc6c229e6248030218b650424
hsw28 <holdbackaglacier@yahoo.com>
2017-08-22 15:20:13 -0400

    more on chunking. should be in a testable state

--- hannah-in-use/inprep/MASSCHUNKvelVsFiringRate.m
+++ hannah-in-use/inprep/MASSCHUNKvelVsFiringRate.m
@@ -16,31 +16,27 @@ while k <= posnum
     date = strsplit(name,'_position');
     date = char(date(1,1)); % now have for ex date_2017_04_28_trial_1_forced
 
-%%RESUME HERE
-
-    % get date of spike
-    date = strsplit(name,'cluster_'); %splitting at year
-    date = char(date(1,2));
-    date = strsplit(date,'_maze_cl');
-    date = char(date(1,1));
-    date = strcat(date, '!');
-    date = regexp(date,'_1!|_2!|_3!|_4!|_5!|_6!|_7!|_8!|_9!|_10!|_11!|_12!|_13!|_14!|_15!|_16!|_17!|_18!|_19!|_20!|_21!|_22!|_23!|_24!|_25!|_26!|_27!|_28!|_29!|_30!|_31!|_32!','split');
-    %date = strsplit(date,'_1!'|'_2!'|'_3!','_4!','_5!','_6!','_7!','_8!','_9!','_10!','_11!','_12!','_13!','_14!','_15!','_16!','_17!','_18!','_19!','_20!','_21!','_22!','_23!','_24!', '_25!', '_26!', '_27!', '_28!', '_29!', '_30!', '_31!', '_32!')
-    date = char(date(1,1));
-    %date = regexp(date,'(?=[maze])_1_|_2_|_3_|_4_|_5_|_6_|_7_|_8_|_9_|_10_|_11_|_12_|_13_|_14_|_15_|_16_|_17_|_18_|_19_|_20_|_21_|_22_|_23_|_24_|_25_|_26_|_27_|_28_|_29_|_30_|_31_|_32_','split');
-    %date = char(date(1,1));
-    % formats date to be same as in position structure: date_2015_08_01_acc
     velformateddate = strcat(date, '_vel');
-    velformateddate = strcat('date_', velformateddate);
-    % formats date to be same as in time structure: date_2015_08_01_time
-    timeformateddate = strcat(date, '_time');
-    timeformateddate = strcat('date_', timeformateddate);
-    % formats date to be same as in time structure: date_2015_08_01_position
+    accformateddate = strcat(date, '_acc');
     posformateddate = strcat(date, '_position');
-    posformateddate = strcat('date_', posformateddate);
+
+    timeformateddate = regexp(date, '_forced|_middle|_reward', 'split');
+    timeformateddate = char(timeformateddate(1,1));
+    timeformateddate = strcat(date, '_time')
+
+    clusterformat = regexp(date, '_forced|_middle|_reward', 'split');
+    clusterformat = char(clusterformat(1,1));
+    clusterformat = strsplit(date, 'date_');
+    clusterformat = (1,2);
+    clusterformat = strcat(date, 'cluster_');
+    % not actually the cluster format since it doesnt have cl_clustnum after it. but not sure i need this section anyway
 
     %get conversion factor
-    numdate = {date};
+    numdate = strsplit(name,'date_');
+    numdate = (1,2); %now have for ex 2017_04_28_trial_1_forced
+    numdate = strsplit(name,'_trial');
+    numdate = (1,1); %now have for ex 2017_04_28
+    numdate = {numdate};
     numdate = char(strrep(numdate,'_',''));
     numdate = str2num(numdate);
       if numdate < 20170427
@@ -50,7 +46,6 @@ while k <= posnum
       else
         conversion = 1;
       end
-
 
     % limit the times in the time file to those in position files
     starttime = posstructure.(posformateddate)(1,1);
@@ -62,35 +57,39 @@ while k <= posnum
     time = [timestructure.(timeformateddate)(starttime:endtime)];
 
 
-    % does the thing
-    % want to decide on output-- maybe number of spikes, slope, and r2 value
-    spikename = char(spikenames(k));
-    %set(0,'DefaultFigureVisible', 'off');
-    accvrate = accelVsFiringRate((time.*conversion), (posstructure.(velformateddate).*conversion), (spikestructure.(spikename).*conversion), windowsize);
-    xlabel('Average Velocity')
-    x = accvrate(:,1);
-    actualvals = find(~isnan(x));
-    x = x(actualvals);
-    y = accvrate(:,2);
-    y = y(actualvals);
-    coeffs = polyfit(x, y, 1);
-    slope = coeffs(1); % get slope of best fit line
-    intercept = coeffs(2);
-    % Get fitted values
-    polydata = polyval(coeffs,x);
-    sstot = sum((y - mean(y)).^2);
-    ssres = sum((y - polydata).^2);
-    rsquared = 1 - (ssres / sstot); % get r^2 value
+    %permute through all clusters and do the thing
+    q = 1;
+    spikenames = fieldnames(spikestructure));
+    spikenums = length(spikenames);
+    while q <= length(spikenums)
+        % does the thing
+        spike = char(spikenames(q));
+        %set(0,'DefaultFigureVisible', 'off');
+        accvrate = accelVsFiringRate((time.*conversion), (posstructure.(velformateddate).*conversion), (spikestructure.(spike).*conversion), windowsize);
+        xlabel('Average Velocity')
+        x = accvrate(:,1);
+        actualvals = find(~isnan(x));
+        x = x(actualvals);
+        y = accvrate(:,2);
+        y = y(actualvals);
+        coeffs = polyfit(x, y, 1);
+        slope = coeffs(1); % get slope of best fit line
+        intercept = coeffs(2);
+        % Get fitted values
+        polydata = polyval(coeffs,x);
+        sstot = sum((y - mean(y)).^2);
+        ssres = sum((y - polydata).^2);
+        rsquared = 1 - (ssres / sstot); % get r^2 value
 
+        spikesizes = spikestructure.(spikename);
+        stats = fitlm(x,y);
+        pval = stats.Coefficients.pValue(2);
 
-    spikesizes = spikestructure.(spikename);
-    stats = fitlm(x,y);
-    pval = stats.Coefficients.pValue(2);
+        % made chart with name, number of spikes, number of points on graph, slope, and r2 value, and p value from t test
+        newdata = {name; length(spikesizes); size(x,1); slope; rsquared; pval};
 
-    % made chart with name, number of spikes, number of points on graph, slope, and r2 value, and p value from t test
-    newdata = {name; length(spikesizes); size(x,1); slope; rsquared; pval};
-
-    output = horzcat(output, newdata);
+        output = horzcat(output, newdata);
+    end
 
     %positions are 3 in a row (forced middle reward), seperated by 6 (vel and acc)
     if k==1 | k==2 | rem(k, 9) == 1 | rem(k, 9) == 2
@@ -98,6 +97,7 @@ while k <= posnum
     else
         k = k+7;
     end
+
   end
 
 % outputs chart with spike name, number of spikes, slope, and r2 value
